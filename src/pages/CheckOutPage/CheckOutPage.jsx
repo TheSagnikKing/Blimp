fdb21db079961db0fd8d7434881172d896f03d23
Sagnik Nandy <sagniknandy27@gmail.com>
2025-12-05 13:15:36 +0530

    checkout page design

--- src/pages/CheckOutPage/CheckOutPage.jsx
+++ src/pages/CheckOutPage/CheckOutPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./CheckOutPage.module.css";
 import {
   CreditCardIcon,
@@ -8,256 +8,251 @@ import {
   WhatsappIcon,
 } from "../../icons";
 import ProgressBar from "../../components/ProgressBar/ProgressBar";
+import RangeSlider from "react-range-slider-input";
+import "react-range-slider-input/dist/style.css";
+import Switch from "react-switch";
 
 const CheckOutPage = () => {
+  const [selectedTab, setSelectedTab] = useState("Give Once");
+  const [openCustomTipInput, setOpenCustomTipInput] = useState(false);
+
+  const [donationCheck, setDonationCheck] = useState(false);
+
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [personalEmail, setPersonalEmail] = useState("");
+
+  const [firstnameError, setFirstnameError] = useState("");
+  const [lastnameError, setLastnameError] = useState("");
+  const [personalEmailError, setPersonalEmailError] = useState("");
+
   return (
     <main>
       <section className={style.checkoutSectionContainer}>
         <div>
-          <h2>Reduce Homelessness</h2>
+          {/* <h2>Reduce Homelessness</h2> */}
 
           <div>
-            <div>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqdnu3tuqGZxqxqvmLhwEEPSCTrXCJOW0J4Q&s"
-                alt=""
+            {/* <img
+              src="https://cdn.sanity.io/images/2ejqxsnu/production/d6965115d7edc08e1417b0a9ae13aca2117c51b0-1280x760.jpg?w=3840&q=75&fit=clip&auto=format"
+              alt=""
+            /> */}
+
+            <div className={style.donationContainer}>
+              <p>
+                Published by: <b>Arghya Ghosh</b>
+              </p>
+
+              <ProgressBar
+                raisedAmount={3000}
+                targetAmount={10000}
+                percentageAchieved={60}
+                donationCount={500000}
+                currency={"dollar"}
+                symbol={"$"}
               />
 
-              <div className={style.donationContainer}>
-                <p>
-                  Published by: <b>Arghya Ghosh</b>
-                </p>
-
-                {/* <div>
-                  <div>
-                    <p>$180,050 USD raised</p>
-                    <p>$200K goal - 3.9K donations</p>
-                  </div>
-
-                  <div className={style.circularProgressbar}>
-                    <div>
-                      <p>100%</p>
-                    </div>
-                  </div>
-                </div> */}
-
-                <ProgressBar />
-
-                <div className={style.linkContainer}>
-                  <div>
-                    <button>
-                      <InstagramIcon />
-                    </button>
-                    <button>
-                      <FacebookIcon />
-                    </button>
-                    <button>
-                      <TwitterXIcon />
-                    </button>
-                    <button>
-                      <WhatsappIcon />
-                    </button>
-                  </div>
-                </div>
-              </div>
-
-              <div action="" className={style.donationForm}>
+              <div className={style.linkContainer}>
                 <div>
-                  <h2>Donation amount</h2>
-                  <div>
-                    <div>
-                      <p>$ 10</p>
-                    </div>
-                    <div>
-                      <p>$ 25</p>
-                    </div>
-                    <div>
-                      <p>$ 50</p>
-                    </div>
-                    <div>
-                      <p>$ 100</p>
-                    </div>
-                    <div>
-                      <p>$ 500</p>
-                    </div>
-                  </div>
-
-                  <div>
-                    <p>$</p>
-                  </div>
+                  <button>
+                    <InstagramIcon />
+                  </button>
+                  <button>
+                    <FacebookIcon />
+                  </button>
+                  <button>
+                    <TwitterXIcon />
+                  </button>
+                  <button>
+                    <WhatsappIcon />
+                  </button>
                 </div>
-
-                <div>
-                  <div>
-                    <p>Paid by Donor:</p>
-                    <p>$ 200</p>
-                  </div>
-
-                  <div>
-                    <p>Payment processing fees:</p>
-                    <p>$ 4.29</p>
-                  </div>
-
-                  <div>
-                    <p>Blimp.com platform fee:(4.9%)</p>
-                    <p>$ 9.80</p>
-                  </div>
-
-                  <div>
-                    <p>Campaign receives:</p>
-                    <p>$ 185.91</p>
-                  </div>
-                </div>
-
-                <div>
-                  <h2>Select Payment Method</h2>
-
-                  <div>
-                    <div>
-                      <input type="radio" />
-                      <p>Credit Card</p>
-                    </div>
-
-                    <div>
-                      <input type="radio" />
-                      <p>UPI</p>
-                    </div>
-                  </div>
-
-                  <div>
-                    <p>Credit Card Number</p>
-                    <div>
-                      <span>
-                        <CreditCardIcon />
-                      </span>
-                      <input type="text" />
-                    </div>
-                  </div>
-                </div>
-
-                <div className={style.personalFormContainer}>
-                  <h2>Personal Information</h2>
-
-                  <div>
-                    <div>
-                      <label htmlFor="firstname">First Name</label>
-                      <input type="text" placeholder="First Name" />
-                    </div>
-
-                    <div>
-                      <label htmlFor="lastname">Last Name</label>
-                      <input type="text" placeholder="Last Name" />
-                    </div>
-                  </div>
-
-                  <div>
-                    <label htmlFor="email">Email Address</label>
-                    <input type="text" placeholder="email" />
-                  </div>
-
-                  <div>
-                    <h2>
-                      Donation Total: <span>$200</span>
-                    </h2>
-                    <button>Donate now</button>
-                  </div>
-                </div>
-              </div>
-
-              <div className={style.liveContainer}>
-                <h2>*Brief real time update about the cause*</h2>
-                <p>
-                  Urna velit pharetra pellentesque magna eget. Ut egestas est id
-                  netus. Facilisis mollis morbi ultrices ac tellus vitae pulvinar.
-                  Egestas sagittis nec et arcu enim ac. Vivamus a dignissim nulla
-                  ornare sit aliquam elementum blandit. Leo in sem pellentesque
-                  viverra malesuada viverra eget aliquam.
-                </p>
-
-                <p>Live</p>
-
-                <img
-                  src="https://redclaycreative.com/wp-content/uploads/blurry-image-scaled.jpg"
-                  alt=""
-                />
-
-                <p>
-                  Vivamus a dignissim nulla ornare sit aliquam elementum blandit.
-                  Leo in sem pellentesque viverra malesuada viverra eget aliquam:
-                </p>
-
-                <ul>
-                  <li>
-                    <b>Nunc tortor et a ornare et placerat.</b> Tellus in
-                    ultricies risus accumsan turpis id nam. Maecenas proin sodales
-                    diam vel mauris facilisis arcu semper. Mi accumsan gravida
-                    dignissim turpis sollicitudin. At auctor sed facilisi massa
-                    amet. Est morbi aliquam sed orci.
-                  </li>
-
-                  <li>
-                    <b>
-                      Pulvinar aliquam sed egestas tempus aliquet sollicitudin.
-                    </b>{" "}
-                    Lectus et rhoncus venenatis interdum lectus nam. Amet
-                    curabitur cursus pulvinar nisl id morbi adipiscing. Nunc eget
-                    arcu enim ac pellentesque integer bibendum augue. Ut amet
-                    tortor auctor hendrerit. Massa at amet nisl mauris vulputate.
-                  </li>
-
-                  <li>
-                    <b>Accumsan quis vel habitasse arcu nisi sed.</b> Pharetra
-                    malesuada velit iaculis urna eu. Luctus lobortis lacus metus
-                    nec ullamcorper. Arcu nisl odio elit nunc. Arcu amet imperdiet
-                    cras volutpat. Facilisis euismod bibendum urna eu feugiat. Et
-                    morbi mauris ultrices massa tellus purus suspendisse nec.
-                    Magnis tempor aliquam elementum imperdiet posuere. Quis arcu
-                    ultricies id quisque leo pulvinar augue sit.
-                  </li>
-
-                  <li>
-                    <b>
-                      Arcu ultricies malesuada lectus nulla est nunc integer
-                      pellentesque magna.
-                    </b>{" "}
-                    Egestas malesuada faucibus arcu nunc elit leo quis interdum.
-                    Ac vel in commodo accumsan mollis cras massa posuere eget.
-                    Condimentum posuere velit cras velit tortor ridiculus sit.
-                    Lectus augue libero etiam sed nisl.
-                  </li>
-                </ul>
               </div>
             </div>
 
-            <div>
-              <div>
-                <h2>Subscribe to our newsletter</h2>
-                <input type="text" placeholder="Enter your email address" />
-                <button>Subscribe</button>
-              </div>
-
-              <div>
-                <h2>Urgent Causes</h2>
-
-                {[0, 1, 2, 3].map((item, index) => {
+            <div className={style.donationFormContainer}>
+              <div className={style.donationTabContainer}>
+                {["Give Once", "Monthly"].map((item) => {
                   return (
-                    <div key={index}>
-                      <img
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSew3VgziU9t5k9kWQHj74v2nPCXkIr9Ke8F1rm-XX7kiI6VR69T2y5zeMlfII12Blc-hQ&usqp=CAU"
-                        alt=""
-                      />
-                      <div>
-                        <h2>End Hunger</h2>
-                        <p>
-                          Ut id velit tempor eu amet nunc. Vestibulum iaculis cras
-                          sed odio. A dolor vitae ultrices{" "}
-                        </p>
-                        <button>view details</button>
-                      </div>
-                    </div>
+                    <button
+                      key={item}
+                      onClick={() => {
+                        setSelectedTab(item);
+                      }}
+                      style={{
+                        backgroundColor:
+                          selectedTab === item ? "#274A34" : "#fff",
+                        color: selectedTab === item ? "#CBF78D" : "#000",
+                      }}
+                    >
+                      {item}
+                    </button>
                   );
                 })}
               </div>
+              <p>Boost your impact by giving monthly</p>
+
+              <div>
+                <div>
+                  <p>$10</p>
+                </div>
+                <div>
+                  <p>$25</p>
+                </div>
+                <div>
+                  <p>$50</p>
+                </div>
+                <div>
+                  <p>$100</p>
+                </div>
+                <div>
+                  <p>$500</p>
+                </div>
+                <div>
+                  <p>$1000</p>
+                </div>
+              </div>
+
+              <div>
+                <p>$</p>
+                <p>1000.00</p>
+              </div>
+
+              <div>
+                <p>Tip Blimp Services</p>
+                <p>
+                  Blimp believes in empowering every organizer — with{" "}
+                  <strong>0% platform fees.</strong> We run on kindness, not
+                  fees. Your optional donation helps us keep this mission alive
+                  and continue supporting those who create positive change.{" "}
+                </p>
+              </div>
+
+              <RangeSlider
+                className="single-thumb"
+                defaultValue={[0, 50]}
+                thumbsDisabled={[true, false]}
+                rangeSlideDisabled={true}
+              />
+
+              <button
+                onClick={() => {
+                  setOpenCustomTipInput((prev) => !prev);
+                }}
+              >
+                Enter custom tip
+              </button>
+
+              {openCustomTipInput && (
+                <input
+                  placeholder="Enter custom input"
+                  className={style.custom_input}
+                />
+              )}
+
+              <div className={style.tip_container}>
+                <p>
+                  Are you able to add a tip? Tips keep Blimp running, so people
+                  like Marjolijn can get the help they need.
+                </p>
+                <div>
+                  <div>
+                    <div>
+                      <p>1.5%</p>
+                    </div>
+                    <div>
+                      <p>3%</p>
+                    </div>
+                    <div>
+                      <p>5%</p>
+                    </div>
+                  </div>
+                  <p>Your tip: $0</p>
+                </div>
+              </div>
+
+              <div className={style.donation_header}>
+                <div>
+                  <Switch
+                    // width={40}
+                    // height={20}
+                    // handleDiameter={14}
+                    offColor="#97A5B4"
+                    onColor="#0A84FF"
+                    readOnly
+                    checked={donationCheck}
+                    onChange={() => {
+                      setDonationCheck((prev) => !prev);
+                    }}
+                  />
+                  <p>Donate Anonymously</p>
+                </div>
+                <p>Turn on to keep your identity private on the fundraiser</p>
+              </div>
+
+              <div className={style.personal_info_container}>
+                <p>Personal Information</p>
+
+                <div>
+                  <label>First Name</label>
+                  <input
+                    type="text"
+                    placeholder="Enter your first name"
+                    value={firstname}
+                    onChange={(e) => setFirstname(e.target.value)}
+                  />
+                  {firstnameError && (
+                    <p className="input-error-message">{firstnameError}</p>
+                  )}
+                </div>
+
+                <div>
+                  <label>Last Name</label>
+                  <input
+                    type="text"
+                    placeholder="Enter your last name"
+                    value={lastname}
+                    onChange={(e) => setLastname(e.target.value)}
+                  />
+                  {lastnameError && (
+                    <p className="input-error-message">{lastnameError}</p>
+                  )}
+                </div>
+
+                <div>
+                  <label>Email ID</label>
+                  <input
+                    type="text"
+                    placeholder="Enter your email ID"
+                    value={personalEmail}
+                    onChange={(e) => setPersonalEmail(e.target.value)}
+                  />
+                  {personalEmailError && (
+                    <p className="input-error-message">{personalEmailError}</p>
+                  )}
+                </div>
+              </div>
+
+              <div className={style.your_donation_container}>
+                  <p>Your donation</p>
+                  <div>
+                    <p>Your donation</p>
+                    <p>$1000.00</p>
+                  </div>
+                  <div>
+                    <p>Blimp tip</p>
+                    <p>$0.00</p>
+                  </div>
+                  <div className={style.separator}/>
+                  <div>
+                    <p>Total due today</p>
+                    <p>$1000</p>
+                  </div>
+                  <p>Your total amount will be charged in the fundraiser's currency, US <strong>($9.00)</strong>. International transaction and conversion fees may apply based on your payment method.</p>
+              </div>
+
+              <button className={style.donate_btn}>Donate</button>
+
             </div>
           </div>
         </div>
