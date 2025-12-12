438095828e21ef85eb170bd98cc077ecfa310a30
Sagnik Nandy <sagniknandy27@gmail.com>
2025-12-12 13:30:42 +0530

    commit

--- src/pages/ForgetChangedPassword/ForgetChangedPassword.jsx
+++ src/pages/ForgetChangedPassword/ForgetChangedPassword.jsx
@@ -1,11 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./ForgetChangedPassword.module.css";
-import { useState } from "react";
+import toast from "react-hot-toast";
+import { toastStyle } from "../../utils/toastStyles";
+import api from "../../api/api";
+import { ClipLoader } from "react-spinners";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const ForgetChangedPassword = () => {
 
-  const [password, setPassword] = useState("")
-  const [passwordError, setPasswordError] = useState("")
+  const navigate = useNavigate()
+  const location = useLocation()
+  const {state} = location
+
+
+  const [password, setPassword] = useState("");
+  const [confirmPasswordValue, setConfirmPasswordValue] = useState("");
+
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
+  const validatePassword = (value) => {
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/.test(value);
+  };
+
+  const [changePasswordLoader, setChangePasswordLoader] = useState(false);
+
+  const handleSubmit = async () => {
+    let hasError = false;
+
+    // Reset errors first
+    setPasswordError("");
+    setConfirmPasswordError("");
+
+    // Password validations
+    if (!password.trim()) {
+      setPasswordError("Password is required");
+      hasError = true;
+    } else if (!validatePassword(password)) {
+      setPasswordError(
+        "Password must be at least 8 chars long & contain uppercase, lowercase, and a number"
+      );
+      hasError = true;
+    }
+
+    // Confirm password validations
+    if (!confirmPasswordValue.trim()) {
+      setConfirmPasswordError("Confirm Password is required");
+      hasError = true;
+    } else if (password !== confirmPasswordValue) {
+      setConfirmPasswordError("Passwords do not match");
+      hasError = true;
+    }
+
+    if (hasError) return;
+
+    try {
+      setChangePasswordLoader(true);
+      const { data } = await api.post(`/change-password/${state.resetToken}`, {
+        password,
+      });
+
+      if (data.code === 200) {
+        toast.success(data.message, {
+          duration: 3000,
+          style: toastStyle,
+        });
+        navigate("/login-signup");
+      } else {
+        toast.error(data.message, { duration: 3000, style: toastStyle });
+      }
+    } catch (error) {
+      toast.error("Forget password failed", {
+        duration: 3000,
+        style: toastStyle,
+      });
+    } finally {
+      setChangePasswordLoader(false);
+    }
+  };
 
   return (
     <main className={style.authContainer}>
@@ -13,6 +85,7 @@ const ForgetChangedPassword = () => {
         <div>
           <h2>Change Password</h2>
 
+          {/* Password */}
           <div>
             <label>Password</label>
             <input
@@ -21,28 +94,37 @@ const ForgetChangedPassword = () => {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
-
             {passwordError && (
               <p className="input-error-message">{passwordError}</p>
             )}
           </div>
 
+          {/* Confirm Password */}
           <div>
             <label>Confirm Password</label>
             <input
               type="password"
-              placeholder="Enter your confirm password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Confirm your password"
+              value={confirmPasswordValue}
+              onChange={(e) => setConfirmPasswordValue(e.target.value)}
             />
-
-            {passwordError && (
-              <p className="input-error-message">{passwordError}</p>
+            {confirmPasswordError && (
+              <p className="input-error-message">{confirmPasswordError}</p>
             )}
           </div>
 
-          <button>Confirm</button>
-
+          <button onClick={handleSubmit}>
+            {changePasswordLoader ? (
+              <ClipLoader
+                color="#fff"
+                size={"3rem"}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+              />
+            ) : (
+              "Confirm"
+            )}
+          </button>
         </div>
       </div>
     </main>
--- src/pages/ForgetPassword/ForgetPassword.jsx
+++ src/pages/ForgetPassword/ForgetPassword.jsx
@@ -1,16 +1,73 @@
 import React, { useState } from "react";
 import style from "./ForgetPassword.module.css";
+import toast from "react-hot-toast";
+import { toastStyle } from "../../utils/toastStyles";
+import api from "../../api/api";
+import { ClipLoader } from "react-spinners";
+import { useNavigate } from "react-router-dom";
 
 const ForgetPassword = () => {
+
+  const navigate = useNavigate()
   const [email, setEmail] = useState("");
   const [emailError, setEmailError] = useState("");
 
-  
+  const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  const [forgetPasswordLoader, setForgetPasswordLoader] = useState(false);
+
+  const ForgetPassword = async () => {
+    let hasError = false;
+
+    setEmailError("");
+
+    if (!email.trim()) {
+      setEmailError("Email is required");
+      hasError = true;
+    } else if (!validateEmail(email)) {
+      setEmailError("Invalid email format");
+      hasError = true;
+    }
+
+    if (hasError) return;
+
+    try {
+      setForgetPasswordLoader(true);
+      const { data } = await api.post("/forget-password", {
+        email,
+      });
+
+      if (data.code === 200) {
+        toast.success(data.message, {
+          duration: 3000,
+          style: toastStyle,
+        });
+        navigate("/forget-changepassword", {
+          state: {
+            resetLink: data?.data?.resetlink,
+            resetToken: data?.data?.resetToken,
+            useremail: email
+          }
+        })
+      } else {
+        toast.error(data.message, { duration: 3000, style: toastStyle });
+      }
+    } catch (error) {
+      toast.error("Forget password failed", {
+        duration: 3000,
+        style: toastStyle,
+      });
+    } finally {
+      setForgetPasswordLoader(false);
+    }
+  };
 
   return (
     <main className={style.authContainer}>
       <div>
-
         <div>
           <h2>Forget Password</h2>
 
@@ -26,9 +83,19 @@ const ForgetPassword = () => {
             {emailError && <p className="input-error-message">{emailError}</p>}
           </div>
 
-          <button>Send</button>
+          <button disabled={forgetPasswordLoader} onClick={ForgetPassword}>
+            {forgetPasswordLoader ? (
+              <ClipLoader
+                color="#fff"
+                size={"3rem"}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+              />
+            ) : (
+              "Send"
+            )}
+          </button>
         </div>
-
       </div>
     </main>
   );
